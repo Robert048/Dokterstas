9280c34589857a74e2a07c69c9e7937b7562b40d
Robert048 <Robertkarate@hotmail.com>
2017-02-07 12:00:46 +0100

    expandable list view

--- app/src/main/java/minor/dokterstas/MainActivity.java
+++ app/src/main/java/minor/dokterstas/MainActivity.java
@@ -1,15 +1,16 @@
 package minor.dokterstas;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SparseArray<Group> groups = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createData();
+        ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
+        ExpandableListAdapter adapter = new ExpandableListAdapter(this,
+                groups);
+        listView.setAdapter(adapter);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    public void createData() {
+        for (int j = 0; j < 5; j++) {
+            Group group = new Group("Test " + j);
+            for (int i = 0; i < 5; i++) {
+                group.children.add("Sub Item" + i);
             }
-        });
+            groups.append(j, group);
+        }
     }
 
     @Override
@@ -42,8 +49,44 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_Inventory) {
+            setContentView(R.layout.content_edit);
+            return true;
+        }
+
+        if (id == R.id.Settings_1) {
+            if(item.isChecked())
+            {
+                item.setChecked(false);
+            }
+            else
+            {
+                item.setChecked(true);
+            }
+            return true;
+        }
+
+        if (id == R.id.Settings_2) {
+            if(item.isChecked())
+            {
+                item.setChecked(false);
+            }
+            else
+            {
+                item.setChecked(true);
+            }
+            return true;
+        }
+
+        if (id == R.id.Settings_3) {
+            if(item.isChecked())
+            {
+                item.setChecked(false);
+            }
+            else
+            {
+                item.setChecked(true);
+            }
             return true;
         }
 
