c55a461d5f1174c4fc8b0de8912c58d699061155
Hakob Khachadurian <hakobk1@gmail.com>
2017-02-07 12:38:23 +0100

    edit page

--- app/src/main/java/minor/dokterstas/EditActivity.java
+++ app/src/main/java/minor/dokterstas/EditActivity.java
@@ -1,13 +1,16 @@
 package minor.dokterstas;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.SparseArray;
+import android.widget.ExpandableListView;
+
+
 
 public class EditActivity extends AppCompatActivity {
+
+    SparseArray<Group> groups = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +18,20 @@ public class EditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createData();
+        ExpandableListView listView = (ExpandableListView) findViewById(R.id.listView);
+        ExpandableListAdapter adapter = new ExpandableListAdapter(this, groups);
+        listView.setAdapter(adapter);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    public void createData() {
+        for (int j = 0; j < 5; j++) {
+            Group group = new Group("Test " + j);
+            for (int i = 0; i < 5; i++) {
+                group.children.add("Sub Item" + i);
             }
-        });
+            groups.append(j, group);
+        }
     }
 
 }
