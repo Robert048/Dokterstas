7376d458924a919c21c24aa32e15b6b9f82332f3
Robert048 <Robertkarate@hotmail.com>
2017-02-08 13:57:07 +0100

    checklist

--- app/src/main/java/minor/dokterstas/MainActivity.java
+++ app/src/main/java/minor/dokterstas/MainActivity.java
@@ -13,7 +13,6 @@ import android.database.Cursor;
 import android.content.Intent;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
