4594de2ff8ce3ff4c5d04e252687f795d2328767
Hakob Khachadurian <hakobk1@gmail.com>
2017-02-09 03:29:20 +0100

    inserted more data

--- app/src/main/java/minor/dokterstas/database/DatabaseHelper.java
+++ app/src/main/java/minor/dokterstas/database/DatabaseHelper.java
@@ -14,10 +14,12 @@ import minor.dokterstas.DatabaseContract;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "BagDatabase";
+    public static final String DATABASE_NAME = "TasDatabase";
+
     public static final String TABLE_CATEGORIES = "category_table";
     public static final String COLUMN_CATEGORIES_ID = "C_ID";
     public static final String COLUMN_CATEGORIES_NAME = "NAME";
+
     public static final String TABLE_ITEMS = "items_table";
     public static final String COLUMN_ITEMS_ID = "I_ID";
     public static final String COLUMN_ITEMS_NAME = "NAME";
@@ -42,6 +44,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + COLUMN_ITEMS_CATEGORIES_ID + " INTEGER, FOREIGN KEY("+ COLUMN_ITEMS_CATEGORIES_ID +") REFERENCES "+ TABLE_CATEGORIES  +"(" + COLUMN_CATEGORIES_ID + ") )");
 
 
+        //INSERT CATEGORIES
         ContentValues values = new ContentValues();
         values.put(COLUMN_CATEGORIES_NAME, "Geneesmiddel");
         db.insert(TABLE_CATEGORIES, null, values);
@@ -50,6 +53,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values2.put(COLUMN_CATEGORIES_NAME, "Injectiemateriaal");
         db.insert(TABLE_CATEGORIES, null, values2);
 
+        ContentValues values3 = new ContentValues();
+        values3.put(COLUMN_CATEGORIES_NAME, "Formulieren");
+        db.insert(TABLE_CATEGORIES, null, values3);
+
+        ContentValues values4 = new ContentValues();
+        values4.put(COLUMN_CATEGORIES_NAME, "Instrumenten");
+        db.insert(TABLE_CATEGORIES, null, values4);
+
+        ContentValues values5 = new ContentValues();
+        values5.put(COLUMN_CATEGORIES_NAME, "Verbandmateriaal");
+        db.insert(TABLE_CATEGORIES, null, values5);
+
+
+        //INSERT DEFAULT ITEMS IN CATEGORIES
         ContentValues itemValues = new ContentValues();
         itemValues.put(COLUMN_ITEMS_NAME, "Injectienaalden");
         itemValues.put(COLUMN_ITEMS_CATEGORIES_ID, 2);
@@ -69,6 +86,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         itemValues4.put(COLUMN_ITEMS_NAME, "Atropine");
         itemValues4.put(COLUMN_ITEMS_CATEGORIES_ID, 1);
         db.insert(TABLE_ITEMS, null, itemValues4);
+
+        ContentValues itemValues5 = new ContentValues();
+        itemValues5.put(COLUMN_ITEMS_NAME, "Pen");
+        itemValues5.put(COLUMN_ITEMS_CATEGORIES_ID, 3);
+        db.insert(TABLE_ITEMS, null, itemValues5);
+
+        ContentValues itemValues6 = new ContentValues();
+        itemValues6.put(COLUMN_ITEMS_NAME, "Receptenpapier");
+        itemValues6.put(COLUMN_ITEMS_CATEGORIES_ID, 3);
+        db.insert(TABLE_ITEMS, null, itemValues6);
+
+        ContentValues itemValues7 = new ContentValues();
+        itemValues7.put(COLUMN_ITEMS_NAME, "Stethoscoop");
+        itemValues7.put(COLUMN_ITEMS_CATEGORIES_ID, 4);
+        db.insert(TABLE_ITEMS, null, itemValues7);
+
+        ContentValues itemValues8 = new ContentValues();
+        itemValues8.put(COLUMN_ITEMS_NAME, "Pleisters");
+        itemValues8.put(COLUMN_ITEMS_CATEGORIES_ID, 5);
+        db.insert(TABLE_ITEMS, null, itemValues8);
     }
 
 
